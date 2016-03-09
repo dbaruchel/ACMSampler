54e630314015ed3dd50ff231bac8000527f526d9
mcoenca <m.coenca@gmail.com>
2016-03-10 00:31:12 +0100

    sound playings

--- ACMSampler.js
+++ ACMSampler.js
@@ -1,4 +1,4 @@
-/*global Touches: true, Directions: true, Sons: true */
+/*global Touches: true, Directions: true, Sons: true, SonsObjects: true, createjs */
 
 // Collection pour communiquer les touches tapees
 Touches = new Mongo.Collection('touches');
@@ -36,8 +36,17 @@ Sons = [
     fileName: 'ACM9_echappee',
   },
 ];
-
 const sonExtension = 'm4a';
+
+SonsObjects = Sons.map((son, idx) => {
+  return {
+    fileName: son.fileName,
+    src: `${son.fileName}.${sonExtension}`,
+    id: idx,
+  };
+});
+
+
 
 if (Meteor.isClient) {
   //Visualisation et ecoute
@@ -83,9 +92,10 @@ if (Meteor.isClient) {
     'directions': () => Directions,
   });
 
-  // On se positionne en cercle pour se la peter
-  // http://stackoverflow.com/questions/8436187/circular-layout-of-html-elements
+
   Template.coeur.rendered = function() {
+    // On se positionne en cercle pour se la peter
+    // http://stackoverflow.com/questions/8436187/circular-layout-of-html-elements
     const directionsElems = this.$('.direction-container');
     const alpha = Math.PI * 2 / Directions.length;
     const x0 = window.innerWidth / 2;
@@ -101,6 +111,52 @@ if (Meteor.isClient) {
       elem.style.left = `${x}px`;
       elem.style.top = `${y}px`;
     });
+    // On Load les sons avec createJS
+    // https://github.com/CreateJS/SoundJS/blob/master/examples/02_PlayOnClick.html
+    const soundLoaded = function(event) {
+      console.log('sound loaded', event.id);
+    };
+
+    const init = function() {
+      if (!createjs.Sound.initializeDefaultPlugins()) {
+        alert('error init createjs');
+        return;
+      }
+
+      const assetsPath = '/';
+      // createjs.Sound.alternateExtensions = ['mp3']; // add other extensions to try loading if the src file extension is not supported
+      createjs.Sound.addEventListener('fileload', createjs.proxy(soundLoaded, this)); // add an event listener for when load is completed
+      createjs.Sound.registerSounds(SonsObjects, assetsPath);
+    };
+
+    init();
+  };
+
+  Template.direction.created = function() {
+    //counter of different sound touches
+    this.touchOccurences = null;
+
+    //Sound player state
+    this.preload = null;
+    this.stop = () => {
+      if (this.preload !== null) {
+        this.preload.close();
+      }
+      createjs.Sound.stop();
+    };
+
+    this.playSound = (target) => {
+      //Play the sound: play (src, interrupt, delay, offset, loop, volume, pan)
+      let instance = createjs.Sound.play(target.id);
+      if (instance === null ||
+        instance.playState === createjs.Sound.PLAY_FAILED) {
+        return;
+      }
+
+      instance.addEventListener('complete', function(instance) {
+        console.log('finished playing');
+      });
+    };
   };
 
   //donnees et events des directions
@@ -112,12 +168,22 @@ if (Meteor.isClient) {
 
   Template.direction.helpers({
     'touchOccurences': function() {
-      return Touches.findOne({name: this.name})
-        .touchOccurences;
+      const temp = Template.instance();
+      if (temp.touchOccurences === null) temp.touchOccurences = 0;
+
+      let currentTouch = Touches.findOne({name: this.name});
+      let touchOccurences = currentTouch.touchOccurences;
+
+      if (touchOccurences !== temp.touchOccurences) {
+        temp.playSound(_.find(SonsObjects,
+          (obj) => obj.fileName === currentTouch.currentTouch));
+      }
+      return touchOccurences;
     },
     'sonCourant': function() {
-      return Touches.findOne({name: this.name})
+      let sonCourant = Touches.findOne({name: this.name})
         .currentTouch;
+      return sonCourant;
     },
   });
 }
